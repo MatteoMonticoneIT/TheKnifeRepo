5725dd46ad0e1d350edd9a2adb486778cfff5664
Dr4k3rg3 <damio@192.168.1.60>
2025-05-26 12:41:23 +0200

    - Revisited Review Class

--- src/theknife/obj/review/Review.java
+++ src/theknife/obj/review/Review.java
@@ -16,7 +16,8 @@ import theknife.obj.lists.ListResponse;
  * @author Matteo Monticone     761701 (CO)
  * @author Mattia Tamburo       761743 (CO)
  */
-@JsonPropertyOrder({
+@JsonPropertyOrder(
+{
     "ID",
     "restaurantID",
     "username", 
@@ -24,44 +25,44 @@ import theknife.obj.lists.ListResponse;
     "content", 
     "responses"
 })
-public final class Review {
-    
+public final class Review 
+{    
     //<editor-fold defaultstate="collapsed" desc="Fields">
     /**
      * The review's id.
      */
     @JsonProperty("ID")
-    private int id;
+    private int             id;
     
     /**
      * The restaurant's id.
      */
     @JsonProperty("restaurantID")
-    private int restaurantID;
+    private int             restaurantID;
     
     /**
      * The username of the review, given by a customer's username or the owner of the restaurant.
      */
     @JsonProperty("username")
-    private String username;
+    private String          username;
     
     /**
      * The content of the review, usually a textual description or feedback.
      */
     @JsonProperty("content")
-    private String content;
+    private String          content;
     
     /**
      * The rating given in the review, typically an integer score.
      */
     @JsonProperty("rating")
-    private double rating;
+    private double          rating;
     
     /**
      * The responses to the review, typically an instance of {@link ListResponse}.
      */
     @JsonProperty("responses")
-    private ListResponse responses;
+    private ListResponse    responses;
     //</editor-fold>
     //<editor-fold defaultstate="collapsed" desc="Constructors">
     /**
@@ -70,8 +71,7 @@ public final class Review {
      * Initializes a new {@code Review} object without any attributes set.
      * </p>
      */
-    public Review() {
-    }
+    public Review() {}
     
     /**
      * Constructor that initializes a {@code Review} object with specific content and rating.
@@ -82,12 +82,13 @@ public final class Review {
      * @param content the content of the review
      * @param rating the rating given in the review
      */
-    public Review(int id, int restaurantID, String username, String content, double rating) {
-        setId(id);
-        setRestaurantID(restaurantID);
-        setUsername(username);
-        setContent(content);
-        setRating(rating);
+    public Review(int id, int restaurantID, String username, String content, double rating) 
+    {
+      setId             (id);
+      setRestaurantID   (restaurantID);
+      setUsername       (username);
+      setContent        (content);
+      setRating         (rating);
     }
     //</editor-fold>
     //<editor-fold defaultstate="collapsed" desc="Getters and Setters">
@@ -96,107 +97,83 @@ public final class Review {
      *
      * @return the review's id
      */
-    public final int getId() {
-        return id;
-    }
+    public final int            getId           ()                          {return id;}
     
-    /**
-     * Sets the review's id.
-     *
-     * @param id the new id to set
-     */
-    public final void setId(int id) {
-        this.id = id;
-    }
-
     /**
      * Returns the restaurant's id.
      *
      * @return the restaurant's id
      */
-    public int getRestaurantID() {
-        return restaurantID;
-    }
-    
-    /**
-     * Sets the restaurant's id.
-     *
-     * @param restaurantID the new restaurant id to set
-     */
-    public void setRestaurantID(int restaurantID) {
-        this.restaurantID = restaurantID;
-    }
+    public       int            getRestaurantID ()                          {return restaurantID;}
     
     /**
      * Returns the username of the review.
      * 
      * @return the username of the review
      */
-    public String getUsername() {
-        return username;
-    }
-
-    /**
-     * Sets the username of the review.
-     * 
-     * @param username the username to set
-     */
-    public void setUsername(String username) {
-        this.username = username;
-    }
+    public       String         getUsername     ()                          {return username;}
     
     /**
      * Returns the content of the review.
      *
      * @return the content of the review
      */
-    public final String getContent() {
-        return content;
-    }
-    
-    /**
-     * Sets the content of the review.
-     *
-     * @param content the content to set for the review
-     */
-    public final void setContent(String content) {
-        this.content = content;
-    }
+    public final String         getContent      ()                          {return content;}
     
     /**
      * Returns the rating assigned in the review.
      *
      * @return the rating of the review
      */
-    public final double getRating() {
-        return rating;
-    }
-    
-    /**
-     * Sets the rating for the review.
-     *
-     * @param rating the rating to assign to the review
-     */
-    public final void setRating(double rating) {
-        this.rating = rating;
-    }
+    public final double         getRating       ()                          {return rating;}
     
     /**
      * Returns the response to the review.
      *
      * @return the response to the review
      */
-    public final ListResponse getResponses() {
-        return responses;
-    }
+    public final ListResponse   getResponses    ()                          {return responses;}
+    
+    /**
+     * Sets the review's id.
+     *
+     * @param id the new id to set
+     */
+    public final void           setId           (int id)                    {this.id            = id;}
+
+    /**
+     * Sets the restaurant's id.
+     *
+     * @param restaurantID the new restaurant id to set
+     */
+    public       void           setRestaurantID (int restaurantID)          {this.restaurantID  = restaurantID;}
+    
+    /**
+     * Sets the username of the review.
+     * 
+     * @param username the username to set
+     */
+    public       void           setUsername     (String username)           {this.username      = username;}
+     
+    /**
+     * Sets the content of the review.
+     *
+     * @param content the content to set for the review
+     */
+    public final void           setContent      (String content)            {this.content       = content;}
+
+    /**
+     * Sets the rating for the review.
+     *
+     * @param rating the rating to assign to the review
+     */
+    public final void           setRating       (double rating)             {this.rating        = rating;}
     
     /**
      * Sets the response to the review.
      *
      * @param responses the {@code ListResponse} to assign to the review
      */
-    public final void setResponses(ListResponse responses) {
-        this.responses = responses;
-    }
+    public final void           setResponses    (ListResponse responses)    {this.responses     = responses;}
     //</editor-fold>
 }
