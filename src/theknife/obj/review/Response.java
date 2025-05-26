9dfe1624bd01ef3b05bdb4386b6db9cc7ebf2747
Dr4k3rg3 <damio@192.168.1.60>
2025-05-26 12:33:05 +0200

    - Revisited Response Class

--- src/theknife/obj/review/Response.java
+++ src/theknife/obj/review/Response.java
@@ -14,45 +14,46 @@ import com.fasterxml.jackson.annotation.JsonPropertyOrder;
  * @author Matteo Monticone     761701 (CO)
  * @author Mattia Tamburo       761743 (CO)
  */
-@JsonPropertyOrder({
+@JsonPropertyOrder(
+{
     "ID",
     "reviewID",
     "username", 
     "content", 
     "review"
 })
-public final class Response {
-    
+public final class Response 
+{   
     //<editor-fold defaultstate="collapsed" desc="Fields">
     /**
      * The review's id.
      */
     @JsonProperty("ID")
-    private int id;
+    private int     id;
     
     /**
      * The restaurant's id.
      */
     @JsonProperty("reviewID")
-    private int reviewID;
+    private int     reviewID;
     
     /**
      * The username of the response, given by a customer's username or the owner of the restaurant.
      */
     @JsonProperty("username")
-    private String username;
+    private String  username;
     
     /**
      * The content of the response.
      */
     @JsonProperty("content")
-    private String content;
+    private String  content;
     
     /**
      * The {@link Review} object that this response is related to.
      */
     @JsonProperty("review")
-    private Review review;
+    private Review  review;
     //</editor-fold>
     //<editor-fold defaultstate="collapsed" desc="Constructors">
     /**
@@ -61,8 +62,7 @@ public final class Response {
      * Initializes a new {@code Response} object without any attributes set.
      * </p>
      */
-    public Response() {
-    }
+    public Response() {}
     
     /**
      * Constructor that initializes the {@code Response} object with specific content and associated review.
@@ -73,12 +73,13 @@ public final class Response {
      * @param content the content of the response
      * @param review the review that this response is related to
      */
-    public Response(int id, int reviewID, String username, String content, Review review) {
-        setId(id);
-        setReviewID(reviewID);
-        setUsername(username);
-        setContent(content);
-        setReview(review);
+    public Response(int id, int reviewID, String username, String content, Review review) 
+    {
+      setId         (id);
+      setReviewID   (reviewID);
+      setUsername   (username);
+      setContent    (content);
+      setReview     (review);
     }
     //</editor-fold>
     //<editor-fold defaultstate="collapsed" desc="Getters and Setters">
@@ -87,89 +88,69 @@ public final class Response {
      *
      * @return the response's id
      */
-    public final int getId() {
-        return id;
-    }
+    public final int    getId       ()                  {return id;}
     
-    /**
-     * Sets the response's id.
-     *
-     * @param id the new id to set
-     */
-    public final void setId(int id) {
-        this.id = id;
-    }
-
     /**
      * Returns the review's id.
      *
      * @return the review's id
      */
-    public int getReviewID() {
-        return reviewID;
-    }
-    
-    /**
-     * Sets the review's id.
-     *
-     * @param reviewID the new review id to set
-     */
-    public void setReviewID(int reviewID) {
-        this.reviewID = reviewID;
-    }
+    public       int    getReviewID ()                  {return reviewID;}
     
     /**
      * Returns the username of the response.
      * 
      * @return the username of the response
      */
-    public String getUsername() {
-        return username;
-    }
-
-    /**
-     * Sets the username of the review.
-     * 
-     * @param username the username to set
-     */
-    public void setUsername(String username) {
-        this.username = username;
-    }
+    public       String getUsername ()                  {return username;}
     
     /**
      * Returns the content of the response.
      *
      * @return the content of the response
      */
-    public final String getContent() {
-        return content;
-    }
-    
-    /**
-     * Sets the content of the response.
-     *
-     * @param content the content to set for the response
-     */
-    public final void setContent(String content) {
-        this.content = content;
-    }
+    public final String getContent  ()                  {return content;}
     
     /**
      * Returns the review that this response is associated with.
      *
      * @return the review that this response is related to
      */
-    public final Review getReview() {
-        return review;
-    }
+    public final Review getReview   ()                  {return review;}
+    
+    /**
+     * Sets the response's id.
+     *
+     * @param id the new id to set
+     */
+    public final void   setId       (int id)            {this.id        = id;}
+
+    /**
+     * Sets the review's id.
+     *
+     * @param reviewID the new review id to set
+     */
+    public       void   setReviewID (int reviewID)      {this.reviewID  = reviewID;}
+    
+    /**
+     * Sets the username of the review.
+     * 
+     * @param username the username to set
+     */
+    public       void   setUsername (String username)   {this.username  = username;}
+     
+    /**
+     * Sets the content of the response.
+     *
+     * @param content the content to set for the response
+     */
+    public final void   setContent  (String content)    {this.content   = content;}
     
     /**
      * Sets the review that this response is associated with.
      *
      * @param review the review to associate with the response
      */
-    public final void setReview(Review review) {
-        this.review = review;
-    }
+    public final void   setReview   (Review review)     {this.review    = review;}
     //</editor-fold>
 }
