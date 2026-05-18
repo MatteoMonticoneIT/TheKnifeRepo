683c3aef2625f5c19bf19ba25c7e7b3d953eda72
ellel <ellel@DESKTOP-R0CAQM2>
2026-05-18 16:16:57 +0200

    TheKnifeServer:   - aggiunti a serverTK i metodi:     - encryptCustomer;     - encryptRestaurateur:     - calculateRatingAverage   - aggiunto il package theknife.obj

---
+++ TheKnifeServer/src/main/java/theknife/obj/InputPattern.java
@@ -0,0 +1,110 @@
+package theknife.obj;
+
+import java.util.regex.Pattern;
+
+/**
+ * A utility class to manage all input patterns.
+ * <p>
+ * This class stores all input patterns used to validate all input required using regular expression also known as regex.
+ * </p>
+ * 
+ * @author Damiano De Mutiis    761348 (CO)
+ * @author Matteo Porto Bonacci 761396 (CO)
+ * @author Matteo Monticone     761701 (CO)
+ * @author Mattia Tamburo       761743 (CO)
+ */
+public final class InputPattern 
+{   
+    //<editor-fold defaultstate="collapsed" desc="Consts">
+    /**
+     * The first name regex.
+     */
+    public static final Pattern FIRST_NAME                          = Pattern.compile("^[\\p{L}][\\p{L}\\p{M}'\\- ]{1,98}[\\p{L}]$");
+    
+    /**
+     * The last name regex.
+     */
+    public static final Pattern LAST_NAME                           = Pattern.compile("^[\\p{L}'-]+( [\\p{L}'-]+)*$");
+    
+    /**
+     * The birth date regex.
+     */
+    public static final Pattern BIRTH_DATE                          = Pattern.compile("^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[0-2])/\\d{4}$");
+    
+    /**
+     * The address regex.
+     */
+    public static final Pattern ADDRESS                             = Pattern.compile("^(?=.{1,256}$)(\\d{1,5}[A-Za-z]?\\s?[\\p{L}\\s\\.'-]+|[\\p{L}\\s\\.'-]+\\s\\d{1,5}[A-Za-z]?)(,\\s?[\\p{L}\\s\\.'-]+){1,3}$");
+    
+    /**
+     * The email regex.
+     */
+    public static final Pattern EMAIL                               = Pattern.compile("^(?=.{6,256}$)[\\w.-]+@[a-zA-Z\\d-]+\\.[a-zA-Z]{2,}$");
+    
+    /**
+     * The username regex.
+     */
+    public static final Pattern USERNAME                            = Pattern.compile("^(?![_.])(?!.*\\.\\.)(?!.*____)[a-zA-Z0-9._]{3,32}(?<![_.])$");
+    
+    /**
+     * The password regex.
+     */
+    public static final Pattern PASSWORD                            = Pattern.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@#$!%*?&\\(\\)])[a-zA-Z\\d@#$!?%*?&\\(\\)\\.,]{8,128}$");
+    
+    /**
+     * The {@code Restaurant} name regex.
+     */
+    public static final Pattern RESTAURANT_NAME                     = Pattern.compile("^[\\p{L}\\p{N}][\\p{L}\\p{N}'&.,\\- ]{1,49}$");
+    
+    /**
+     * The {@code Restaurant} address regex.
+     */
+    public static final Pattern RESTAURANT_ADDRESS                  = Pattern.compile("^(?=.{1,256}$)(\\d{1,5}[A-Za-z]?\\s?[\\p{L}\\s\\.'-]+|[\\p{L}\\s\\.'-]+\\s\\d{1,5}[A-Za-z]?)(,\\s?[\\p{L}\\s\\.'-]+){1,3}$");
+    
+    /**
+     * The {@code Restaurant} country regex.
+     */
+    public static final Pattern RESTAURANT_COUNTRY                  = Pattern.compile("^(?=.{2,70}$)\\p{L}(?:[\\p{L}'\\- ]*\\p{L})$");
+    
+    /**
+     * The {@code Restaurant} city regex.
+     */
+    public static final Pattern RESTAURANT_CITY                     = Pattern.compile("^(?=.{2,70}$)\\p{L}(?:[\\p{L}'\\-\\. ]*\\p{L})$");
+    
+    /**
+     * The {@code Restaurant} latitude and longitude regex.
+     */
+    public static final Pattern RESTAURANT_LATITUDE_AND_LONGITUDE   = Pattern.compile("^[\\d\\.\\+\\-]{1,15}$");
+    
+    /**
+     * The {@code Restaurant} currency regex.
+     */
+    public static final Pattern RESTAURANT_CURRENCY                 = Pattern.compile("^[\\p{Sc}]$");
+    
+    /**
+     * The {@code Restaurant} phone number regex.
+     */
+    public static final Pattern RESTAURANT_PHONE_NUMBER             = Pattern.compile("^\\+?[\\d\\s\\-\\(\\)\\.]{7,20}$");
+    
+    /**
+     * The {@code Restaurant} URL regex.
+     */
+    public static final Pattern RESTAURANT_URL                      = Pattern.compile("^(https?):\\/\\/(www\\.)?([a-zA-Z0-9_-]+\\.)+[a-zA-Z]{2,}(:\\d{1,5})?(\\/[^\\s?#]*)?(\\?[^\\s#]*)?(#\\S*)?$");
+    
+    /**
+     * The {@code Restaurant} Website URL regex.
+     */
+    public static final Pattern RESTAURANT_WEBURL                   = Pattern.compile("^(https?):\\/\\/(([a-zA-Z0-9_-]+\\.)+[a-zA-Z]{2,})(\\/[^\\s]*)?$");
+
+    //</editor-fold>
+    //<editor-fold defaultstate="collapsed" desc="Methods">
+    /**
+     * Matches a pattern with an input.
+     *
+     * @param pattern the pattern to use
+     * @param input the input to validate
+     * @return true if the validation is ok, false otherwise
+     */
+    public static final boolean match (Pattern pattern, String input) {return pattern.matcher(input).matches();}
+    //</editor-fold>
+}
--- TheKnifeServer/src/main/java/theknife/server/serverTK.java
+++ TheKnifeServer/src/main/java/theknife/server/serverTK.java
@@ -1,12 +1,21 @@
 package theknife.server;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import simple.crypto.AES;
+import simple.logging.LoggerUtils;
+import theknife.obj.AppPaths;
 
 /**
  * 
@@ -18,8 +27,23 @@ import java.util.Scanner;
 
 public class serverTK 
 {
+  //<editor-fold defaultstate="collapsed" desc="Fields">
+  /**
+   * The {@link AES} which encrypt/decrypt data.
+   */
+    
+  private static       AES  aes;
+  
+  /**
+   * KeyStore file for encryption.
+   */
+  private static final File KEYSTORE_FILE         = AppPaths.getRequiredFile("data", "keystore.jks");
+  
+  //</editor-fold>
+  
   public static void main(String[] args) 
   {
+    aes = new AES(KEYSTORE_FILE);
     Scanner scanner = new Scanner(System.in);
 
     System.out.println("=====================================\n" +
@@ -27,15 +51,15 @@ public class serverTK
                        "=====================================\n");
 
     System.out.print("Inserisci l'host del DB (localhost:5432): ");
-    String dbHost = scanner.nextLine();
+    String dbHost       = scanner.nextLine();
 
     System.out.print("Inserisci l'username del database the_knife_db (postgres): ");
-    String dbUser = scanner.nextLine();
+    String dbUser       = scanner.nextLine();
 
     System.out.print("Inserisci la password del database the_knife_db (1234): ");
-    String dbPassword = scanner.nextLine(); 
+    String dbPassword   = scanner.nextLine(); 
 
-    String jdbcUrl = "jdbc:postgresql://" + dbHost + "/the_knife_db"; 
+    String jdbcUrl      = "jdbc:postgresql://" + dbHost + "/the_knife_db"; 
 
     try 
     {
@@ -80,4 +104,238 @@ public class serverTK
       System.err.println("[ERRORE SERVER] Eccezione nell'avvio del server: " + e.getMessage());
     }
   }
+  
+  //<editor-fold defaultstate="collapsed" desc="Exclusive Programmer Methods">
+    /**    
+     * @param dbConnection
+     * @throws Exception 
+     * Method to encrypt all Customers passwords
+     */
+    private static void encryptCustomer(Connection dbConnection) throws Exception
+    {
+      ArrayList<Integer>    IDs         = new ArrayList<>();
+      ArrayList<String>     passwords   = new ArrayList<>();
+      
+      String sql = "SELECT id, password FROM customer";
+       
+      try (PreparedStatement pstmt = dbConnection.prepareStatement(sql)) 
+      {  
+        // 4. Esecuzione della query e lettura dei risultati
+        try (ResultSet rs = pstmt.executeQuery()) 
+        {
+          // Cicla attraverso tutte le righe restituite
+          while (rs.next()) 
+          {
+            IDs         .add        (rs.getInt      ("id"));
+            passwords   .add        (rs.getString   ("password"));           
+            passwords   .set        (passwords.size()-1, aes.encrypt(passwords.get(passwords.size()-1)));            
+          }
+        }
+      }
+      catch (SQLException e) 
+      {
+        System.err.println("[ERRORE] Impossibile connettersi al database.");
+        System.err.println("Motivo: " + e.getMessage());
+        System.exit(1);
+      } 
+      
+      sql = "UPDATE customer SET password = ? WHERE id = ?";
+
+      try (PreparedStatement pstmt = dbConnection.prepareStatement(sql)) 
+      {
+        dbConnection.setAutoCommit(false);
+        for (int i = 0; i < IDs.size(); i++) 
+        {
+          pstmt.setString   (1, passwords.get(i)); 
+          pstmt.setInt      (2, IDs.get(i));          
+          pstmt.addBatch    ();
+        }
+        int[] risultati = pstmt.executeBatch();
+        dbConnection.commit();
+      } 
+      catch (SQLException e) 
+      {
+        try 
+        {
+          if (dbConnection != null) 
+            dbConnection.rollback();
+        } 
+        catch (SQLException ex) 
+        {
+          ex.printStackTrace();
+        }
+        e.printStackTrace();
+      } 
+      finally 
+      {
+        try 
+        {
+          if (dbConnection != null) 
+            dbConnection.setAutoCommit(true);
+        } 
+        catch (SQLException ex) 
+        {
+          ex.printStackTrace();
+        }
+      }       
+    }
+    
+    /**
+     * 
+     * @param dbConnection
+     * @throws Exception 
+     * Method to encrypt all restaurateurs passwords
+     */
+    private static void encryptRestaurateur(Connection dbConnection) throws Exception
+    {
+      ArrayList<Integer>    IDs         = new ArrayList<>();
+      ArrayList<String>     passwords   = new ArrayList<>();
+      
+      String sql = "SELECT id, password FROM restaurateur";
+       
+      try (PreparedStatement pstmt = dbConnection.prepareStatement(sql)) 
+      {  
+        try (ResultSet rs = pstmt.executeQuery()) 
+        {
+          while (rs.next()) 
+          {
+            IDs         .add        (rs.getInt      ("id"));
+            passwords   .add        (rs.getString   ("password"));           
+            passwords   .set        (passwords.size()-1, aes.encrypt(passwords.get(passwords.size()-1)));            
+          }
+        }
+      }
+      catch (SQLException e) 
+      {
+        System.err.println("[ERRORE] Impossibile connettersi al database.");
+        System.err.println("Motivo: " + e.getMessage());
+        System.exit(1);
+      } 
+      
+      sql = "UPDATE restaurateur SET password = ? WHERE id = ?";
+
+      try (PreparedStatement pstmt = dbConnection.prepareStatement(sql)) 
+      {
+        dbConnection.setAutoCommit(false);
+        for (int i = 0; i < IDs.size(); i++) 
+        {
+          pstmt     .setString      (1, passwords.get(i)); 
+          pstmt     .setInt         (2, IDs.get(i));          
+          pstmt     .addBatch       ();
+        }
+        pstmt       .executeBatch   ();
+        dbConnection.commit         ();
+      } 
+      catch (SQLException e) 
+      {
+        try 
+        {
+          if (dbConnection != null) 
+            dbConnection.rollback();
+        } 
+        catch (SQLException ex) 
+        {
+          ex.printStackTrace();
+        }
+        e.printStackTrace();
+      } 
+      finally 
+      {
+        try 
+        {
+          if (dbConnection != null) 
+            dbConnection.setAutoCommit(true);
+        } 
+        catch (SQLException ex) 
+        {
+          ex.printStackTrace();
+        }
+      }      
+    }
+    
+    /**
+     * Method to calculate all restaurant rating average.
+     */
+    private static void calculateRatingAverage(Connection dbConnection) throws Exception
+    {
+      ArrayList<Double>     averageReview   = new ArrayList<>();
+      ArrayList<Integer>    restaurantsID   = new ArrayList<>();  
+      
+      String sql = "SELECT restaurantID, rating FROM Review ORDER BY restaurantID";
+       
+      try (PreparedStatement pstmt = dbConnection.prepareStatement(sql)) 
+      {  
+        // 4. Esecuzione della query e lettura dei risultati
+        try (ResultSet rs = pstmt.executeQuery()) 
+        {
+          if(rs.next())
+          {
+            int     temp  = rs.getInt("restaurantID");
+            int     count = 1;
+            double  somma = rs.getDouble("rating");
+            
+            while (rs.next()) 
+            {
+              if(temp != rs.getInt("restaurantID"))
+              {               
+                averageReview.add(Math.round((somma/count)*100.0)/100.0);
+                restaurantsID.add(temp);
+                somma = 0;
+                count = 0;
+                temp = rs.getInt("restaurantID");
+              }
+              somma += rs.getDouble("rating");
+              count += 1;
+            }
+          }
+        }
+      }
+      catch (SQLException e) 
+      {
+        System.err.println("[ERRORE] Impossibile connettersi al database.");
+        System.err.println("Motivo: " + e.getMessage());
+        System.exit(1);
+      } 
+      
+      sql = "UPDATE restaurant SET rating = ? WHERE id = ?";
+
+      try (PreparedStatement pstmt = dbConnection.prepareStatement(sql)) 
+      {
+        dbConnection.setAutoCommit(false);
+        for (int i = 0; i < restaurantsID.size(); i++) 
+        {
+          pstmt     .setDouble      (1, averageReview.get(i)); 
+          pstmt     .setInt         (2, restaurantsID.get(i));          
+          pstmt     .addBatch       ();
+        }
+        pstmt       .executeBatch   ();
+        dbConnection.commit         ();
+      } 
+      catch (SQLException e) 
+      {
+        try 
+        {
+          if (dbConnection != null) 
+            dbConnection.rollback();
+        } 
+        catch (SQLException ex) 
+        {
+          ex.printStackTrace();
+        }
+        e.printStackTrace();
+      } 
+      finally 
+      {
+        try 
+        {
+          if (dbConnection != null) 
+            dbConnection.setAutoCommit(true);
+        } 
+        catch (SQLException ex) 
+        {
+          ex.printStackTrace();
+        }
+      }  
+    }
+    //</editor-fold>
 }
